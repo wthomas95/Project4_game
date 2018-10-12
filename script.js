74a59b094d55a629911bad7d27ec9c40532ab222
unknown <holden.beale@gmail.com>
2018-10-12 13:03:21 -0400

    touches

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ let Character = {
     artifacts: [0, 0, 0],
     health: 100,
     missChance: 0,
-    damage: 7,
-    dealt: 0
+    damage: 0,
 }
 let Enemy = {
     name: "",
@@ -16,7 +15,7 @@ var mugged = 0;
 var turns;
 var elfCount;
 var nextFunc;
-var stun;
+var stun = false;
 var b1 = document.getElementById("c1");
 var b2 = document.getElementById("c2");
 var b3 = document.getElementById("c3");
@@ -24,21 +23,19 @@ var b4 = document.getElementById("c4");
 var header = document.getElementById("t");
 var description = document.getElementById("d");
 var numOfArtifacts = document.getElementById("c");
+var dmgstat = document.getElementById("ds");
 function inventory(){
-    if(Character.artifacts == [0, 0, 0]){
-        numOfArtifacts.innerHTML = 'Artifacts:\n 0/3';
+    dmgstat.innerHTML = `Character Damage:<br>${Character.damage}/successful attack`;
+    var amtOfArtifacts = 0;
+    for(var i = 0;i<Character.artifacts.length;i++){
+        if(Character.artifacts[i] == 1)
+        {
+            amtOfArtifacts++;
+        }
     }
-    else if (Character.artifacts == [1, 0, 0] || Character.artifacts == [0, 1, 0] ||Character.artifacts == [0, 0, 1])
-    {
-        numOfArtifacts.innerHTML = 'Artifacts:\n 1/3';
-    }
-    else if (Character.artifacts == [1, 1, 0] || Character.artifacts == [0, 1, 1] ||Character.artifacts == [1, 0, 1])
-    {
-        numOfArtifacts.innerHTML = 'Artifacts:\n 2/3';
-    }
-    else if (Character.artifacts == [1, 1, 1])
-    {
-        numOfArtifacts.innerHTML = 'Artifacts:\n 3/3, time to return to the stronghold and confront the final boss';
+    numOfArtifacts.innerHTML = `Artifacts:<br>${amtOfArtifacts}/3`;
+    if(amtOfArtifacts == 3){
+        numOfArtifacts.innerHTML = `Artifacts:<br>${amtOfArtifacts}/3<br> you are ready to face the final boss`;    
     }
 }
 function removeListener(){
@@ -52,7 +49,7 @@ function removeListener(){
     }
 }
 function GameStart(){
-    mugged = 0;
+    upgrade = 0;
     elfCount = 0;
     Character.artifacts = [0, 0, 0];
     inventory();
@@ -81,14 +78,14 @@ function victory(){
 }
 function prelude(){
     header.innerHTML = "Prelude";
-    description.innerHTML = "A description of the world and races: \n Orcs: Orcs are a more tribalistic people. They are large and stury, heavily muscled under green tinted hide. Large horns curl up above their heads. These hulks tend to lack finesse when it comes to combat, most warriors prefer to use hammers and other blunt weapons where they can use their great strength to the best effect.\n Dwarves: Dwarves are short, sturdy, and quite strong, though not so much as the orcs. Due to their stature, they are not typically as agile as humans or elves. Most Dwarven warriors prefer to use heavy battleaxes that require less finess than a sword or bow, but more than the Orcish warhammers.Their capitol is in a large mountain, hollowed out ages ago by their ancestors. Past their capitol, many smaller dwarf towns and cities exist, but non-dwarves rarely venture there due to the uncomfortably small tunnels that lead from their capitol to their other cities.\n Humans: Humans live in the north west of the continent. Humans have 3 major cities, a central capitol just north of the mountains leading to the dwarf capitol, a coastal city to the west, and a cooperative city with the elves to the north. Other small villages are dotted around the countryside.\n Elves: Elves are similar to size and strength as humans. They live in the northern forests, and are the most friendly to the orcs. Although it seems that the hyper-refinement of the elves would clash with the roughness of the dwarves, it seems that their shared love of wilderness has brought their races closer together.";
+    description.innerHTML = "A description of the world and races:<br>Orcs: Orcs are a more tribalistic people. They are large and stury, heavily muscled under green tinted hide. Large horns curl up above their heads. These hulks tend to lack finesse when it comes to combat, most warriors prefer to use hammers and other blunt weapons where they can use their great strength to the best effect.<br>Dwarves: Dwarves are short, sturdy, and quite strong, though not so much as the orcs. Due to their stature, they are not typically as agile as humans or elves. Most Dwarven warriors prefer to use heavy battleaxes that require less finess than a sword or bow, but more than the Orcish warhammers.Their capitol is in a large mountain, hollowed out ages ago by their ancestors. Past their capitol, many smaller dwarf towns and cities exist, but non-dwarves rarely venture there due to the uncomfortably small tunnels that lead from their capitol to their other cities.<br>Humans: Humans live in the north west of the continent. Humans have 3 major cities, a central capitol just north of the mountains leading to the dwarf capitol, a coastal city to the west, and a cooperative city with the elves to the north. Other small villages are dotted around the countryside.<br>Elves: Elves are similar to size and strength as humans. They live in the northern forests, and are the most friendly to the orcs. Although it seems that the hyper-refinement of the elves would clash with the roughness of the dwarves, it seems that their shared love of wilderness has brought their races closer together.";
     b1.innerHTML = "continue";
     b1.addEventListener("click", CharSetup);
 }
 function CharSetup(){
     b1.removeEventListener("click", CharSetup);
     header.innerHTML = "Character Setup";
-    description.innerHTML = "Choose between an Orc, Dwarf, Elf, or Human. Orcs and Dwarfs do higher damage but have a chance to miss. Humans do medium damage with no advantages or drawbacks. Elfs do lower damage but get 3 free attacks before the enemy can engage.";
+    description.innerHTML = "Choose between an Orc, Dwarf, Elf, or Human. <br>Orcs and Dwarfs do higher damage but have a chance to miss. <br>Humans do medium damage with no advantages or drawbacks. <br>Elfs do lower damage but get 3 free attacks before the enemy can engage.";
     b1.addEventListener("click", setOrc);
     b2.addEventListener("click", setElf);
     b3.addEventListener("click", setHuman);
@@ -120,6 +117,8 @@ function setDwarf(){
 }
 function loc1(){
     removeListener();
+    upgradeCheck();
+    inventory();
     header.innerHTML = "Orc Stronghold";
     description.innerHTML = `You are a(n) ${Character.race}. The great cheiftain who governs the stronghold has conveined a council of peoples from all races. You sit in the room as people of all races file in. The last person to sit is a middle aged archaeologist, holding a large bundle of scrolls. At the behest of the cheiftain, the archaeologist stands and describes a failed expedition in the far east. An ancient monster was awoken by digging in an ancient tomb. The creature killed the guards and was only held back by a last minute spell that the mages of the expedition sacrificed themselves to cast, a spell so draining that it killed the mages casting it. The archaeologist warns that the spell won't hold for long, and that the only way to stop the monster is to use three ancient artifacts to drain the monster's power and imprison it in the tomb once again. The orc cheiftain stands, addressing warriors of each race in the room,asking who will take the task of finding the artifacts to force the monster back into the crypt.`;
     b1.addEventListener("click", proceed);
@@ -191,6 +190,7 @@ function DwarfThrone(){
     else if (Character.upgrade == true){
         description.innerHTML = "The throne room is empty, only flickering candle light can be seen against the stone walls";
     }
+    inventory();
     b1.addEventListener("click", DwarfCap);
     b1.innerHTML = "Return to the main capitol";
     b2.innerHTML = "";
@@ -241,11 +241,18 @@ function upgradeCheck(){
         }
         if (Character.race === "elf")
         {
+            if (elfCount < 3)
+            {
             Character.damage = 5;
+            }
+            else{
+            Character.damage = 7;
+            console.log("dmg up");
+            }
         }
         if (Character.race === "human")
         {
-            Character.damage = 7;
+            Character.damage = 9;
         }
         if (Character.race === "dwarf")
         {
@@ -259,53 +266,57 @@ function upgradeCheck(){
         }
         if (Character.race === "elf")
         {
+            if (elfCount < 3)
+            {
             Character.damage = 5 * 1.5;
+            }
+            else{
+                Character.damage = 7 * 1.5;
+                console.log("dmg up");
+                }
         }
         if (Character.race === "human")
         {
-            Character.damage = 7 * 1.5;
+            Character.damage = 9 * 1.5;
         }
         if (Character.race === "dwarf")
         {
             Character.damage = 12 * 1.5;
         } 
     }
+    inventory();
 }
 function stunCalc(){
     var stunchance = 0;
     if (Character.race == "orc"){
-        stunChance = Math.random(1, 4);
-        if (stunChance == 1) //30% stun chance for orcs
+        stunChance = Math.floor(Math.random() * 3);
+        if (stunChance == 0) //30% stun chance for orcs
         {  
-            Character.dealt = 0;
             stun = true;
+            console.log("stunned");
         }
         else
         {
             stun = false;
-            Character.dealt = Character.damage;
+            console.log("not stunned");
         }
     }
     else if (Character.race == "dwarf"){
-        stunChance = Math.random(1, 6); //20% stun chance for dwarfs
-        if (stunChance == 1)
+        stunChance = Math.floor(Math.random() * 4); //20% stun chance for dwarfs
+        if (stunChance == 0)
         {
             stun = true;
-            Character.damage = 0;
         }
         else
         {
             stun = false;
-            Character.dealt = Character.damage;
         }
     }
     else if (Character.race == "human"){
             stun = false;
-            Character.dealt = Character.damage;
     }
     else if (Character.race == "elf"){
             stun = false;
-            Character.dealt = Character.damage;
     }
 }
 function turn(){
@@ -338,14 +349,14 @@ function turn(){
             console.log(3);
         }
         else if (turns > 0){
-            Enemy.health -= Character.damage;
             Character.health -= Enemy.damage;
             if (stun == true){
                 description.innerHTML = `On turn ${turns}, you were stunned and dealt no damage. ${Enemy.name} dealt ${Enemy.damage} to you. <br>You: ${Character.health} remaining <br>${Enemy.name}: ${Enemy.health} remaining`;
             }
-            else{
-            description.innerHTML = `On turn ${turns}, you dealt ${Character.dealt} to ${Enemy.name}. ${Enemy.name} dealt ${Enemy.damage} to you. <br>You: ${Character.health} remaining
-            <br>${Enemy.name}: ${Enemy.health} remaining`;
+            else if (stun == false){
+            Enemy.health -= Character.damage;
+            description.innerHTML = `On turn ${turns}, you dealt ${Character.damage} to ${Enemy.name}. ${Enemy.name} dealt ${Enemy.damage} to you. <br>You: ${Character.health} health remaining
+            <br>${Enemy.name}: ${Enemy.health} health remaining`;
             }
             turns++;
         }
@@ -367,8 +378,6 @@ function turn(){
     }
     else if (Character.health <= 0)
     {
-        b1.removeEventListener("click", Character.damage = Character.damage * 1.5);
-        b2.removeEventListener("click", Character.damage = Character.damage);
         header.innerHTML = "Defeat";
         description.innerHTML = "You are defeated by your opponent "
         b1.innerHTML = "Continue";
@@ -419,6 +428,7 @@ function ElfCity(){
     b1.innerHTML = "Orc Stronghold";
     b2.innerHTML = "Human Capitol";
     b3.innerHTML = "Elven Treasury";
+    b4.innerHTML = "";
     b1.addEventListener("click", stronghold);
     b2.addEventListener("click", humCap);
     b3.addEventListener("click", elfCollect);
@@ -451,6 +461,7 @@ function toBoss(){
     Enemy.damage = 10;
     nextFunc = victory;
     turns = 0;
+    elfCount = 0;
     header.innerHTML = "Final Boss: <br> The monster stands in your path, frozen by a field of energy. It's glowing eyes glare down at you while its stony figure stands in front of you. Massive boulders make up its body, each suspended several feet apart by glowing orange energy."
     if (Character.artifacts == [1, 1, 1])
     {
@@ -464,7 +475,11 @@ function toBoss(){
     else if (Character.artifacts != [1, 1, 1])
     {
         description.innerHTML = "You raise the artifacts and shout. A blinding light flashes between the golem and the artifacts. Massive amounts of orange energy flow from the monster into the artifacts, and the monster shrinks down, until it is only the size of an average Orc."
-        turn();
+        b1.innerHTML = "Attack";
+        b2.innerHTML = "";
+        b3.innerHTML = "";
+        b4.innerHTML = "";
+        b1.addEventListener("click", turn);
     }
 }
 function end(){
